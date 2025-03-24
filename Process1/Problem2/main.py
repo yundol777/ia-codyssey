cd253470cfb97fe7c838d69154237fc351e5b28e
정윤석 <mar0722@naver.com>
2025-03-24 12:14:59 +0900

    [문제2] 미션 컴퓨터의 내용을 좀 더 분석해 보자

--- Process1/Problem2/main.py
+++ Process1/Problem2/main.py
@@ -2,13 +2,13 @@ try:
     #1. 파일 읽어들여 출력
     print("1. 파일 읽어들여 출력")
 
-    with open('../Problem1/mission_computer_main.log', 'r', encoding='utf-8') as f:
+    with open('./mission_computer_main.log', 'r', encoding='utf-8') as f:
         print(f.read())
     
     #2 콤마 기준으로 리스트 객체로 전환 후 출력
     print("2. 콤마 기준으로 리스트 객체로 전환 후 출력")
 
-    with open('../Problem1/mission_computer_main.log', 'r', encoding='utf-8') as f:
+    with open('./mission_computer_main.log', 'r', encoding='utf-8') as f:
         lines = f.readlines()[1:]
 
     lines_lists = []
